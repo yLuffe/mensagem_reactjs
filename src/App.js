87126b8f2e8526ae1207e42b3b56cbe7d03d6f60
yLuffe <132414913+yLuffe@users.noreply.github.com>
2023-08-15 21:12:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 
+const endereco_servidor = "https://mensagem-nodejs-vercel.vercel.app";
+
 class FrmMensagem extends Component {
-  constructor(props) { // Construtor
+
+  // Construtor
+  constructor(props) {
     super(props);
+
     // Estados inciais das variáveis do formulário
     this.state = {
       mensagemAnterior: '',
@@ -16,30 +21,33 @@ class FrmMensagem extends Component {
   handleInputChange = (event) => {
     // Recupera o nome e o valor do campo do evento
     const { name, value } = event.target;
-    this.setState({ [name]: value }); // Atualiza o estado
+    // Atualiza o estado
+    this.setState({ [name]: value });
   };
 
-  // Evento da submissão do formulário
+  // Evento da submissão do formulário 
   handleSubmit = (event) => {
-    event.preventDefault(); // Impede o recarregamento da página
-    //Recupera e atualiza a mensagem anterior antes de
-    //enviar a nova mensagem
-    fetch(`http://localhost:8080/mensagem`)
-      .then((response) => response.json()) //Converte para JSON
+    // Impede o recarregamento da página
+    event.preventDefault();
+
+    //Recupera e atualiza a mensagem anterior antes de enviar a nova mensagem
+    fetch(`${endereco_servidor}/mensagem`)
+      .then((response) => response.json()) //Converte a resposta para JSON
       .then((data) => { //Recupera a resposta
-        this.setState({ mensagemAnterior: data.mensagem });
+        this.setState({ mensagemAnterior: data.mensagem }); // Atribui a resposta ao estado
       });
 
-    const { mensagem } = this.state; // Recupera o valor da mensagem
+    // Recupera o valor da mensagem
+    const { mensagem } = this.state;
     // Envia a mensagem nova para o servidor
-    fetch(`http://localhost:8080/mensagempath/${mensagem}`)
-      .then((response) => response.json()) //Converte para JSON
+    fetch(`${endereco_servidor}/mensagempath/${mensagem}`)
+      .then((response) => response.json()) //Converte a resposta para JSON
       .then((data) => { //Recupera a resposta
-        this.setState({ resposta: data.mensagem });
+        this.setState({ resposta: data.mensagem }); // Atribui a resposta ao estado
       });
   };
 
-  // Limpa os campos do formulário.
+  // Limpa os campos do formulário.     
   limpar = () => {
     this.setState({
       mensagemAnterior: '',
@@ -48,25 +56,21 @@ class FrmMensagem extends Component {
     });
   };
 
-  render() {// Renderiza o formulário
+  // Renderiza o formulário
+  render() {
     return (
-      <form name="FrmMensagem" method="get"
-        onSubmit={this.handleSubmit}>
+      <form name="FrmMensagem" method="get" onSubmit={this.handleSubmit}>
         <label><h1>Formulário Mensagem</h1> </label>
-        <label>Mensagem anterior: {this.state.mensagemAnterior}
-        </label><br /><br />
+        <label>Mensagem anterior: {this.state.mensagemAnterior} </label><br /><br />
         <label>Nova mensagem:
-          <input type="text" name="mensagem"
-            value={this.state.mensagem}
-            onChange={this.handleInputChange} />
+          <input type="text" name="mensagem" value={this.state.mensagem} onChange={this.handleInputChange} />
         </label><br />
-        <input type="button" name="Limpar"
-          value="Limpar" onClick={this.limpar} />
-        <input type="submit" name="Enviar"
-          value="Enviar" /><br /><br />
+        <input type="button" name="Limpar" value="Limpar" onClick={this.limpar} />
+        <input type="submit" name="Enviar" value="Enviar" /><br /><br />
         <label>Resposta: {this.state.resposta} </label>
       </form>
     )
   }
 }
+
 export default FrmMensagem;
